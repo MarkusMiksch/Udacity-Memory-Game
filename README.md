db635479497aff142fb59745743649113b1f9e78
markusmiksch <markus.miksch@gmx.at>
2018-03-18 21:57:02 +0100

    first commit

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
 # Memory Game Project
 
-## Table of Contents
-
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+This is a simple Memory Game. Every move you can turn up two cards. If they match they will remain face up. If not, both will be turned down again. You have won the game when all cards are face up. 
+The amount of time and moves it took will determin your rating (1-3 stars).
 
-To get started, open `js/app.js` and start building out the app's functionality
+The project consists of HTML, CSS and JavaScript. No extern librarys are needed.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+Some of the modal code is from w3schools.com
--- js/app.js
+++ js/app.js
@@ -1,38 +1,169 @@
-/*
- * Create a list that holds all of your cards
- */
+// variables for timer
+let time = document.getElementById("time-watch");
+let sec = 0;
+let min = 0;
+let hour = 0;
 
+// this flag is needed for the timer (to stop it when game is finished)
+let winFlag = false;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// variables and eventlisteners for restart
+let restarter = document.getElementById("do-restart");
+restarter.addEventListener("click", restart);
+let modalRestart = document.getElementById("modal-restart");
+modalRestart.addEventListener("click", restart);
+
+// variables for the 3 stars in the header
+let star3 = document.getElementById("star3");
+let star2 = document.getElementById("star2");
+let star1 = document.getElementById("star1");
+
+// variables for shuffling
+let openCards = [];
+let moveCounter = 0;
+let deck = document.querySelector(".deck");
+let imageArray = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt",
+	"fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb",
+	"fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"
+];
+
+// variables eventlisteners for modal
+const modal = document.getElementById("myModal");
+let modalText = document.getElementById("modal-text");
+let modalFooterText = document.getElementById("modal-footer-text");
+const span = document.getElementsByClassName("close")[0];
+span.addEventListener("click", function () {
+	modal.style.display = "none";
+});
+
+document.addEventListener("DOMContentLoaded", giveNewImages);
+deck.addEventListener('click', lookAtCard);
+
+startTime();
+
+// this function assigns the shuffled image-names to the cards
+function giveNewImages() {
+	let shuffledImageArray = shuffle(imageArray);
+	let i = 0;
+	for (let elem of document.getElementsByClassName("card")) {
+		elem.childNodes[1].className = "fa " + shuffledImageArray[i];
+		i++;
+	}
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
+function lookAtCard(event) {
+	// to enable only clicks on the single "li´s", not the whole "ul"
+	if (event.target.nodeName === 'LI') {
+		if (!openCards.includes(event.target)) {
+			// add "open" and "show" to class of target
+			event.target.classList.add("open", "show");
+			// add target to list of open cards
+			openCards.push(event.target);
+			// if even number of cards are open
+			if ((openCards.length % 2 === 0) && (openCards.length > 0)) {
+				// and last 2 cards match: set them to "match"
+				if (openCards[openCards.length - 1].childNodes[1].classList.value == openCards[openCards.length - 2].childNodes[1].classList.value) {
+					setLastCardsToMatch();
+				}
+				// if last 2 cards don't match, face them down after 1sec:
+				else {
+					// remove and add (in Line 106) EventListener to disable clicks while waiting
+					deck.removeEventListener('click', lookAtCard);
+					setTimeout(setLastCardFaceDown, 1000);
+					setTimeout(setLastCardFaceDown, 1000);
+				}
+				incrementMoves();
+				// if FINISHED the game
+				if (openCards.length == 16) {
+					win();
+				};
+			};
+		};
+	};
+};
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function setLastCardsToMatch() {
+	openCards[openCards.length - 1].classList.add("match");
+	openCards[openCards.length - 2].classList.add("match");
+}
+
+function setLastCardFaceDown() {
+	openCards[openCards.length - 1].classList.remove("open", "show");
+	openCards.pop();
+	// remove(in Line 83) and add EventListener to disable clicks while waiting
+	deck.addEventListener('click', lookAtCard);
+}
+
+function incrementMoves() {
+	moveCounter++;
+	document.querySelector(".moves").textContent = moveCounter;
+	if (moveCounter > 15) {
+		star3.className = "fa fa-star-o";
+	};
+	if (moveCounter > 20) {
+		star2.className = "fa fa-star-o";
+	};
+	if (moveCounter > 25) {
+		star1.className = "fa fa-star-o";
+	};
+}
+
+function win() {
+	winFlag = true;
+	modalText.textContent = `It took you ${moveCounter} moves in ${min} Min and ${sec} Secs to finish the game!`;
+	modalFooterText.textContent = `Want to play again?`;
+	modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+// time
+function startTime() {
+	sec++;
+	if (sec >= 60) {
+		sec = 0;
+		min++;
+		if (min >= 60) {
+			min = 0;
+			hour++;
+		};
+	};
+	// to get the Time look like 03:12:07
+	if (sec < 10 && min < 10 && hour < 10) {
+		time.innerHTML = "0" + hour + ":0" + min + ":0" + sec;
+	} else if (min < 10 && hour < 10) {
+		time.innerHTML = "0" + hour + ":0" + min + ":" + sec;
+	} else if (sec < 10 && hour < 10) {
+		time.innerHTML = "0" + hour + ":" + min + ":0" + sec;
+	} else {
+		time.innerHTML = "0" + hour + ":" + min + ":" + sec;
+	}
+	// timer should stop after the game has finished
+	if (!winFlag) {
+		let t = setTimeout(startTime, 1000);
+	};
+}
+
+// restart the game -> from Modal-Button or from the Header Restart-Button
+function restart() {
+	location.reload();
+}
